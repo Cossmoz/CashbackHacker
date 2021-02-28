ba64547aaa3e12d9fbe451ff7255b30858c32ec8
Constantin <zamotaev.post@gmail.com>
2021-02-28 09:02:14 +0400

    tests added

--- src/test/java/ru/netology/domain/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/domain/CashbackHackServiceTest.java
@@ -1,4 +1,61 @@
+package ru.netology.domain;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
+
 public class CashbackHackServiceTest {
-    
+
+    CashbackHackService service = new CashbackHackService();
+
+    @org.testng.annotations.Test
+    public void shouldReturn100() {
+
+        int amount = 900;
+        int expected = 100;
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected);
+
+    }
+
+    @org.testng.annotations.Test
+    public void shouldReturn400() {
+
+        int amount = 1600;
+        int expected = 400;
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected);
+
+    }
+
+    @org.testng.annotations.Test
+    public void shouldReturn4() {
+
+        int amount = 999;
+        int expected = 1;
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected);
+
+    }
+
+    @org.testng.annotations.Test
+    public void shouldReturn999() {
+
+        int amount = 1;
+        int expected = 999;
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected);
+
+    }
+
+    @org.testng.annotations.Test
+    public void shouldReturn0() {
+
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        Assert.assertEquals(actual, expected);
+
+    }
 }
